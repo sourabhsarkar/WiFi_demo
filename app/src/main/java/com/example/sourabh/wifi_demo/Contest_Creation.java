c8684c7e12286b2d55e946832604d5fe298ba5fb
susobhanroy <susoroy.golu@gmail.com>
2016-03-15 12:45:08 +0530

    Null Checking not working

--- app/src/main/java/com/example/sourabh/wifi_demo/Contest_Creation.java
+++ app/src/main/java/com/example/sourabh/wifi_demo/Contest_Creation.java
@@ -4,19 +4,36 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Contest_Creation extends Activity {
 
     Intent intent;
 
+    public static final String TAG = "Contest_Creation";
+
+    TextView con_name;
+    TextView con_password;
+    TextView con_details;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contest__creation);
+        con_name= (TextView)findViewById(R.id.con_name);
+        con_password= (TextView)findViewById(R.id.con_password);
+        con_details= (TextView)findViewById(R.id.con_details);
     }
 
     public void con_create_click(View v){
-        intent = new Intent(this,Contest_Upload.class);
-        startActivity(intent);
+        
+        if(con_name.getText()== null || con_password.getText()== null|| con_details.getText()==null){
+            Toast.makeText(this,"One or more fields is empty",Toast.LENGTH_LONG).show();
+        }
+        else {
+            intent = new Intent(this, Contest_Upload.class);
+            startActivity(intent);
+        }
     }
 }
